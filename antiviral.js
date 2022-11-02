7754719d7b91295bcfc7f00d7a20a6b56d201444
Hao Chen <inbox@haochen.name>
2022-11-02 06:19:39 -0500

    minor

--- antiviral.js
+++ antiviral.js
@@ -16,7 +16,7 @@ var markdownContent = ''
 + "\n"
 + "Please use the live online version at http://chen42.github.io/slides/antiviral.html\n"
 + "\n"
-+ "Or download the [**Printable version**](http://chen42.github.io/slides/antiviral.html?print-pdf#/) before class.\n"
++ "Or download the [**Printable version**](http://chen42.github.io/slides/antiviral.html?print-pdf#/).\n"
 + "\n"
 + "<b>Last update: Nov 2nd, 2022</b>\n"
 + "\n"
